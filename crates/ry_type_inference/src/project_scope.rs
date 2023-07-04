debe5838ac5ce914c691f6dfee98986ffdbd98bf
Adi Salimgereev <adisalimgereev@gmail.com>
2023-07-04 18:02:30 +0600

    feat(inference): add API for project scope
    In the future project scope will be used to store data about
    the project, information in its configuration file, its modules, etc.

--- crates/ry_type_inference/src/project_scope.rs
+++ crates/ry_type_inference/src/project_scope.rs
@@ -1,1 +1,23 @@
+//! Defines a [`ProjectInterface`] to work with Ry projects.
 
+use ry_interner::Symbol;
+use std::path;
+
+/// An interface that allows to interact with Ry projects.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ProjectInterface<'path> {
+    /// Canonical path to the project.
+    path: &'path path::Path,
+
+    /// Data about the project.
+    metadata: ProjectMetadata,
+}
+
+/// All information about a project that is stored in its configuration file.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ProjectMetadata {
+    /// Names (interned symbols) of other projects that the project depends on.
+    depedencies: Vec<Symbol>,
+
+    // TODO
+}
