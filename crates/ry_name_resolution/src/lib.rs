d0618ac40f1cba67c02e6db6ac8d667c2b4aeffa
Adi Salimgereev <adisalimgereev@gmail.com>
2023-07-13 19:28:19 +0600

    feat: add fx hashmap

--- crates/ry_name_resolution/src/lib.rs
+++ crates/ry_name_resolution/src/lib.rs
@@ -1,1 +1,14 @@
+use ry_fx_hash::FxHashMap;
+use ry_interner::Symbol;
 
+pub struct ModuleData<'a> {
+    project: Symbol,
+    name: Symbol,
+    parent: Option<&'a ModuleData<'a>>,
+    bindings: FxHashMap<Symbol, NameBindingData<'a>>,
+}
+
+pub enum NameBindingData<'m> {
+    Module(&'m ModuleData<'m>),
+    Analyzed(),
+}
