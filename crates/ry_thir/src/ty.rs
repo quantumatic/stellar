06011451936e0fcf894cdb5868f0a88b19412902
Adi Salimgereev <adisalimgereev@gmail.com>
2023-08-16 01:30:12 +0600

    refactor(thir): use better macro for generating builtin types functions

--- crates/ry_thir/src/ty.rs
+++ crates/ry_thir/src/ty.rs
@@ -97,29 +97,25 @@ impl TypeVariable {
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct TypeVariableID(pub usize);
 
-macro_rules! t {
-    ($name:ident, $symbol:ident) => {
-        #[inline]
-        #[must_use]
-        #[doc = concat!("Returns a `", stringify!($name), "` type.")]
-        pub fn $name() -> Type {
-            Type::Constructor(TypeConstructor::primitive(builtin_identifiers::$symbol))
-        }
+macro_rules! builtin_types {
+    ($($name:ident => $symbol:ident),*) => {
+        $(
+            #[inline]
+            #[must_use]
+            #[doc = concat!("Returns a `", stringify!($name), "` type.")]
+            pub fn $name() -> Type {
+                Type::Constructor(TypeConstructor::primitive(builtin_identifiers::$symbol))
+            }
+        )*
     };
 }
 
-t!(int8, INT8);
-t!(int16, INT16);
-t!(int32, INT32);
-t!(int64, INT64);
-t!(uint8, UINT8);
-t!(uint16, UINT16);
-t!(uint32, UINT32);
-t!(uint64, UINT64);
-t!(float32, FLOAT32);
-t!(float64, FLOAT64);
-t!(char, CHAR);
-t!(string, STRING);
+builtin_types! {
+    int8 => INT8, int16 => INT16, int32 => INT32, int64 => INT64,
+    uint8 => UINT8, uint16 => UINT16, uint32 => UINT32, uint64 => UINT64,
+    float32 => FLOAT32, float64 => FLOAT64,
+    char => CHAR, string => STRING
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct TypeConstructor {
